d1f44b4c87990bc3507dba4c5ca81a727b863b7c
tm9704 <tm9704@korea.ac.kr>
2022-10-03 22:19:21 +0900

    React Diary: Add Diary List Data

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -33,7 +33,13 @@ const DiaryEditor = () => {
       return;
     }
 
+    onCreate(state.author, state.content, state.emotion);
     alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    });
   };
 
   return (
