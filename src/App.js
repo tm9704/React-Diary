bc6747297549c7bc53db46f14e7be29a4294bfca
tm9704 <tm9704@korea.ac.kr>
2022-10-08 17:22:46 +0900

    React Diary: Last Optimization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
-import { useMemo, useEffect, useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -18,7 +17,7 @@ const App = () => {
         author: it.email,
         content: it.body,
         emotion: Math.floor(Math.random() * 5) + 1,
-        created_date: new Date().getTime() + 1,
+        created_date: new Date().getTime(),
         id: dataId.current++,
       };
     });
@@ -32,7 +31,7 @@ const App = () => {
     }, 1500);
   }, []);
 
-  const onCreate = (author, content, emotion) => {
+  const onCreate = useCallback((author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -41,9 +40,10 @@ const App = () => {
       created_date,
       id: dataId.current,
     };
+
     dataId.current += 1;
-    setData([newItem, ...data]);
-  };
+    setData((data) => [newItem, ...data]);
+  }, []);
 
   const onRemove = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
@@ -59,10 +59,14 @@ const App = () => {
   };
 
   const getDiaryAnalysis = useMemo(() => {
+    if (data.length === 0) {
+      return { goodcount: 0, badCount: 0, goodRatio: 0 };
+    }
+    console.log("일기 분석 시작");
+
     const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
-    const goodRatio = (goodCount / data.length) * 100;
-
+    const goodRatio = (goodCount / data.length) * 100.0;
     return { goodCount, badCount, goodRatio };
   }, [data.length]);
 
@@ -70,12 +74,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
-      <div>전체 일기: {data.length}</div>
-      <div>기분 좋은 일기 개수: {goodCount}</div>
-      <div>기분 나쁜 일기 개수: {badCount}</div>
-      <div>기분 좋은 일기 비율: {goodRatio}</div>
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const DiaryEditor = ({ onCreate }) => {
+const DiaryEditor = React.memo(({ onCreate }) => {
+  useEffect(() => {
+    console.log("DiaryEditor 렌더");
+  });
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -9,9 +12,6 @@ const DiaryEditor = ({ onCreate }) => {
     content: "",
     emotion: 1,
   });
-
-  //   const [author, setAuthor] = useState("");
-  //   const [content, setContent] = useState("");
 
   const handleChangeState = (e) => {
     setState({
@@ -22,13 +22,11 @@ const DiaryEditor = ({ onCreate }) => {
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
-      //focus
       authorInput.current.focus();
       return;
     }
 
     if (state.content.length < 5) {
-      //focus
       contentInput.current.focus();
       return;
     }
@@ -48,30 +46,25 @@ const DiaryEditor = ({ onCreate }) => {
       <div>
         <input
           ref={authorInput}
-          type="text"
-          name="author"
           value={state.author}
           onChange={handleChangeState}
+          name="author"
+          placeholder="작성자"
+          type="text"
         />
       </div>
       <div>
         <textarea
           ref={contentInput}
-          name="content"
-          type="text"
           value={state.content}
           onChange={handleChangeState}
-          //   (e) => {
-          //     setState({
-          //       ...state, //길어지면 복잡하고 힘드니까 spread 연산자 사용
-          //       //순서를 바꾸면 안됨, 위에서 순서대로 새로운 객체를 만들어내기 때문
-          //       //   author: state.author,
-          //       content: e.target.value,
-          //     });
-          //   }
+          name="content"
+          placeholder="일기"
+          type="text"
         />
       </div>
       <div>
+        <span>오늘의 감정점수 : </span>
         <select
           name="emotion"
           value={state.emotion}
@@ -89,42 +82,5 @@ const DiaryEditor = ({ onCreate }) => {
       </div>
     </div>
   );
-};
-
+});
 export default DiaryEditor;
-
-//사용자의 입력을 리액트에서 처리하기 위해서는 state를 사용해야됨
-//상태는 상태변화함수로만 변화시킬 수 있음
-//input에서 실시간으로 반영시키기 위해서는 onChange 속성 사용
-//onChange에 전달되는건 콜백함수, 이 콜백함수는 이벤트 객체 e를 매개변수로 전달받음
-//onChage는 Event라고 부를 수 있음 이벤트는 사용자가 어떤 행동을 한 것
-// input태그에는 사용자가 입력을함 즉, 입력이 발생하면 input은 어떠한 사건이 발생했다라고 여김
-// 해당 이벤트는 onChange라는 이벤트, onChange는 값이 바뀌였을 때 일어나는 이벤트
-// 값이 바뀌면 console.log() 실행
-// onChange={(e) => {
-//     console.log(e);
-//   }}
-// e 는 이벤트 객체인데 변하는 값은 target.value가 변함 (target은 태그), value는 값
-
-// 이벤트 객체는 target의 value 뿐만 아니라 input태그에  name이 지정되어 있었다 하면
-// 타겟의 이름까지 출력이 가능 e.target.name
-
-//textArea 태그는 여러줄을 입력받을 수 있는 태그
-//사용 방법은 input 태그와 동일함
-
-// onChage이벤트가 발생하면 콜백함수가 실행되고 매개변수 e에서 지금 변화한 값을
-// 새로운 상태의 값으로 변화하면서 다시 value가 바뀌고 화면에 반영이 된다.
-
-// 동작이 비슷한 State는 하나의 State로 묶을 수 있음
-// onChage도 함칠 수 있음
-
-// DOM요소를 선택할 수 있는 기능을 리액트가 포함
-// useRef
-// useRef()함수의 리턴은 React.MutableRefObject
-// MutableRefObject는 html 같은 DOM 요소를 접근할 수 있는 기능
-
-// authorInput.current.focus()
-// DOM요소를 선택하는 ref객체는 현재 가리키는 값을 current라는 프로퍼티로 가져와서 사용할 수 있음
-
-//React에서 리스트 렌더링 하기
-// - 배열을 이용해서 React에서 List 렌더링해보고 개별적인 컴포넌트 만들기
